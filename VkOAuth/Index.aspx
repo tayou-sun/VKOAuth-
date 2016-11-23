7182b8825a326eb5f3ab5bade7542c9e3a1b028b
Steven Byerley <Steven Byerley>
2016-11-23 13:03:44 +0300

    new

---
+++ VkOAuth/Index.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="VkOAuth.Index1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <!-- Возможно: стоит проверять Sessin["UserId"] -->
+        <% if (Session["UserId"] == null)
+           { %>
+          <asp:Button id="AuthButton"
+           Text="Click me for greeting..."
+           OnClick="AuthButton_Click" 
+           runat="server"/>
+        <% }
+           else
+           { %>
+            Привет, <%:Session["FirstName"]%>  <%:Session["LastName"]%><br>
+            UserID: <%:Session["UserID"]%><br>
+            With acess token: <%:Session["AccessToken"]%>    
+        <% } %>
+    </form>
+</body>
+</html>
---
+++ VkOAuth/Index.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Web.UI;
+using Newtonsoft.Json;
+
+namespace VkOAuth
+
+{
+
+    public partial class Index1 : Page
+    {
+        //Получать остальные параметры и формировать ссылки: используя их (client id, client secret и redirect uri)
+        private readonly string ClientId = ConfigurationManager.AppSettings["ClientId"];
+        private readonly string ClientSecret = ConfigurationManager.AppSettings["ClientSecret"];
+        private readonly string ResponseUri = ConfigurationManager.AppSettings["ResponseUri"];
+
+        //code-behind
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GetAuthData();
+        }
+
+        protected void AuthButton_Click(object sender, EventArgs e)
+        {
+             Response.Redirect("https://api.vkontakte.ru/oauth/authorize?client_id="+ 
+                 ClientId + "&redirect_uri=" + 
+                 ResponseUri+"&scope=offline&messages&response_type=code&display=page");
+
+        }
+
+      
+
+        private void GetAuthData()
+        {
+            if (Request["code"] != null)
+            {
+                Session["code"] = Request["code"];
+                GetUserInfo();
+            }
+        }
+
+        private void GetUserInfo()
+        {
+            var request =
+                WebRequest.Create(
+                    "https://oauth.vk.com/access_token?client_id="+ClientId + 
+                    "&client_secret="+ClientSecret + "&code=" +
+                    Session["Code"] + "&redirect_uri="+ ResponseUri);
+
+            
+
+            //Try-catch - ловить эксепшн?
+            try
+            {
+                var response = request.GetResponse();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+          
+
+            
+            var line = new StreamReader(request.GetResponse().GetResponseStream()).ReadToEnd();
+
+            dynamic json = JsonConvert.DeserializeObject(line);
+
+            var userId = json.user_id;
+            var accessToken = json.access_token;
+
+            //Получаем строку (похоже: в JSON)
+            //var matches = Regex.Match(line, "\"access_token\":\"([^\"]+)");
+            //var accessToken = matches.Groups[1].Value;
+
+            //matches = Regex.Match(line, "\"user_id\":(\\d+)");
+            //var userId = matches.Groups[1].Value;
+
+            //Проверить на ошибки
+
+            Session["AccessToken"] = accessToken;
+
+            Session["UserId"] = userId;
+
+            var request1 =
+                WebRequest.Create("https://api.vk.com/method/users.get?user_id=" +
+                        Session["UserId"] + "&access_token=" + Session["AccessToken"]);
+            var line1 = new StreamReader(request1.GetResponse().GetResponseStream()).ReadToEnd();
+            dynamic json1 = JsonConvert.DeserializeObject(line1);
+
+
+            foreach (var j in json1["response"].Children())
+            {
+                Session["FirstName"] = j.first_name;
+
+                Session["LastName"] = j.last_name;   
+            }
+
+
+
+           
+
+
+        }
+    }
+}
